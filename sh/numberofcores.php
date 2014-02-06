90d044c142b8d567c2383580ebf60ccdacdf9817
Ian Merin <merinian@gmail.com>
2014-02-05 21:11:21 -0500

    fixed numberofcores for OSX

--- sh/numberofcores.php
+++ sh/numberofcores.php
@@ -1,6 +1,9 @@
 <?php 
 
-    exec('/bin/grep -c ^processor /proc/cpuinfo',$result);
+    exec('/usr/sbin/sysctl machdep.cpu.core_count',$result);
+    //$cores = explode(": ", $result[0]);
+    preg_match('\d', $result[0], $cores)
     header('Content-Type: application/json; charset=UTF-8');
-    echo json_encode( $result[0] );
+    echo json_encode( $cores[0] );
     
+?>
